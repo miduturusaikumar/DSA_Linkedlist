c1da27be16a9b180f245a21d4997e81ab6c31ba8
sai <saikumar13052001@gmail.com>
2023-12-26 20:51:07 +0530

    search element in ll

---
+++ SearchElell.java
@@ -0,0 +1,56 @@
+
+//Length of Likedlist
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)   //creating Node constructor for last element
+    {
+        this.data=data;
+        this.next=null;
+    }
+    Node(int data,Node next)  //creating Node constructor for attaching data address
+    {
+        this.data=data;
+        this.next=next;
+    }
+}
+public class SearchElell
+{
+   static Node convertLL(int arr[])
+   {
+       Node head=new Node(arr[0]);
+       Node mover=head;
+       for(int i=1;i<arr.length;i++)
+       {
+           Node temp=new Node(arr[i]);
+           mover.next=temp;
+           mover=temp;
+       }
+       return head;
+       
+   }
+
+   static int searchEle(Node head,int value)
+   {
+       while(head!=null)
+       {
+           if(head.data==value)
+           {
+               return 1;
+           }
+           head=head.next;
+       }
+       return 0;
+   }
+
+	public static void main(String[] args) {
+	      int arr[]={2,3,4,5,6};
+	      Node head=convertLL(arr);
+	      Node temp=head;
+	      System.out.print(searchEle(head,3));
+	      
+	}
+}
+
